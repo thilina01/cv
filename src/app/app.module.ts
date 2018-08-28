9de2b5a6ab983da096b4f2550a8b404fdfe7d180
gayan <gayankurukulasooriya@gmail.com>
2018-08-28 15:53:44 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { SummaryComponent } from './summary/summary.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExtraComponent } from './extra/extra.component';
+import { NonRelatedComponent } from './non-related/non-related.component';
+import { CertificationComponent } from './certification/certification.component';
+import { TechHistoryComponent } from './tech-history/tech-history.component';
+import { PublicationComponent } from './publication/publication.component';
+import { InterestedComponent } from './interested/interested.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeadComponent,
+    SummaryComponent,
+    TechnologiesComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ProjectsComponent,
+    ExtraComponent,
+    NonRelatedComponent,
+    CertificationComponent,
+    TechHistoryComponent,
+    PublicationComponent,
+    InterestedComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/experience/experience.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-experience',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.css']
+})
+export class ExperienceComponent {
+  @Input() experienceData: any;
+ 
+}
