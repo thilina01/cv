728b2ef3149e22f5a13b01d82c19bf7a730a9263
Thilina Ranathunga <r.thilina@gmail.com>
2018-09-02 18:49:57 +0530

    added default user image feature

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-head',
@@ -8,5 +9,27 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeadComponent {
 
   @Input() headData: any;
+  imageDefaultUrl = "assets/img/user-default.png";
+  imageUrl = "assets/img/user.png";
+
+  constructor(private http: HttpClient) { }
+
+  getImage() {
+    return this.http.get(this.imageUrl);
+  }
+
+  getDefaultImage() {
+    return this.http.get(this.imageDefaultUrl);
+  }
+
+  ngOnInit() {
+    this.getImage()
+      .subscribe((data: any) => {
+      }, error => {
+        if (error.status === 404) {
+          this.imageUrl = this.imageDefaultUrl;
+        }
+      });
+  }
 
 }
